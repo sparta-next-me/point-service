758e3b275a3adedd9544ad00789cc15e06101488
hoonssac <Hoonssac.dev@gmail.com>
2025-12-04 15:14:24 +0900

    Initial commit

---
+++ src/test/java/org/nextme/point_service/PointServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nextme.point_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
